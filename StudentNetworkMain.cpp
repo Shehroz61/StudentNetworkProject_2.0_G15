f78097e879e131dfbf307685681e95458cf43595
Shehroz Hameed <dangerousadmiofficial@gmail.com>
2024-12-06 11:40:13 +0500

    StudentNetworkMain.exe new change

--- StudentNetworkMain.cpp
+++ StudentNetworkMain.cpp
@@ -1,41 +1,59 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include <sstream>
 using namespace std;
-
-void popularfriend(string arr[], int count[]) {
-	for (int i = 0; arr[i] != ""; i++) {
-		for (int j = 0; arr[j] != ""; j++) {
-			if (arr[i] == arr[j]) count[i]++;
+void popularfriend(string arr1[], int arr2[], int studentCount, int totalCount){
+	// first   loop is to store no of friendship and also represent stundents  
+	for (int i=0;i<studentCount;i++){
+		int count=0;
+		//this loop is to check for no of friendship
+		for (int j=studentCount;j< totalCount;j++){
+			if (arr1[i]==arr1[j]){
+				count++;
+			}
 		}
+		arr2[i]=count; 
 	}
-	int maxIndex = 0;
-	for (int i = 1; arr[i] != ""; i++) {
-		if (count[i] > count[maxIndex]) maxIndex = i;
+// this is to check for larger number of friendship and also to find index of student who has most friend
+	int index,larger=INT_MIN;
+	for(int i=0;i<studentCount;i++){
+		if (arr2[i]>larger)
+		{
+			larger=arr2[i];
+			index=i;
+		}   
 	}
-	cout << "Popular student = " << arr[maxIndex] << " with " << count[maxIndex] << " friends" << endl;
+	cout<<"popular student is "<<arr1[index]<<" with "<<larger<<" friend";
 }
-
-int main() {
-	const int maxEntries = 200;
-	// const int maxEntries = 200;
-	string arr[maxEntries] = {""};
-	int count[maxEntries] = {0};
-	ifstream file("friends_dataset.csv");
-	if (!file.is_open()) return cout << "Error: Could not open the file!" << endl, 1;
+int main (){
+	//first initinalize students name than add their friends name
+	int const TOTAL_COUNT=240;
+	string arr1[TOTAL_COUNT];
 	
-	string line;
-	getline(file, line); // Skip header
-	for (int i = 0; getline(file, line) && i < maxEntries; i++) {
-		size_t pos = 0;
-		while ((pos = line.find(',')) != string::npos) {
-			arr[i++] = line.substr(0, pos);
-			line.erase(0, pos + 1);
+	int count = 0;
+	ifstream file("friends_dataset.csv");
+	if (file.is_open()) {
+		string line;
+		
+		while (getline(file, line) && count < TOTAL_COUNT) {
+			stringstream ss(line);
+			string item;
+			
+			// Tokenize the line by comma and store in the array
+			while (getline(ss, item, ',') && count < TOTAL_COUNT) {
+				arr1[count++] = item;
+			}
 		}
-		arr[i] = line; // Last name
+		file.close();
+	} else {
+		cout << "Unable to open file" << endl;
+		return 1;
 	}
-	file.close();
-	popularfriend(arr, count);
+	//second array to store no of friendships
+	int const STUDENT_COUNT=39;
+	int arr2[STUDENT_COUNT];
+	//function to form popular friend
+	popularfriend(arr1,arr2,STUDENT_COUNT,TOTAL_COUNT);
 	return 0;
+	
 }
-
